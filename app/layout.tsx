ebe1145c87bc5cca76aa964e3922d02a101faf2b
hamzamodfx <hamza@modfxmedia.com>
2026-09-16 21:26:38 +0500

    added microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -142,6 +142,13 @@ export default function RootLayout({
             gtag('config', 'G-NTLDFH6CYS');
           `}
         </Script>
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "yj6lraerbd");
+</script>
         <JsonLd data={SITE_GRAPH} />
         <IntroSplash />
         <Header />
