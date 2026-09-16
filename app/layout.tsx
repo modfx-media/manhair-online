6657efc2868a1e1d87ad9debb7506e5bddb705c0
Nargis Fatima <nargis@modfxmedia.com>
2026-09-16 22:15:32 +0500

    Microsoft clarity code

--- app/layout.tsx
+++ app/layout.tsx
@@ -142,6 +142,15 @@ export default function RootLayout({
             gtag('config', 'G-NTLDFH6CYS');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "yj6lraerbd");
+          `}
+        </Script>
         <JsonLd data={SITE_GRAPH} />
         <IntroSplash />
         <Header />
